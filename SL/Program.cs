c1f6753956ef1e4824ed379f176ba24b496352b3
Elios <eliosjb@outlook.com>
2026-06-15 16:30:08 -0600

    Almacenando los 3 usuarios en la lista

--- SL/Program.cs
+++ SL/Program.cs
@@ -1,6 +1,9 @@
+using ML;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<BL.User>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,5 +22,85 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var initializer = scope.ServiceProvider.GetRequiredService<BL.User>();
+
+    initializer.Add(
+        new ML.User
+        {
+            email = "juan@outlook.com",
+            name = "Juan Perez",
+            phone = "5587654321",
+            password = "Juan456",
+            addresses = new List<Address>{
+                       new Address{
+                        id = 1,
+                        name = "Workaddress",
+                        street = "Street NO 1",
+                        country_code = "UK"
+                       },
+                       new Address{
+                        id = 2,
+                        name = "Homeaddress",
+                        street = "Street NO 2",
+                        country_code = "AU"
+                       }
+
+                }
+        }
+            );
+    initializer.Add(
+        new ML.User
+        {
+            email = "maria@outlook.com",
+            name = "Maria Lopez",
+            phone = "5544455566",
+            password = "Maria789",
+            addresses = new List<Address>{
+                        new Address{
+                            id = 1,
+                            name = "Homeaddress",
+                            street = "Street NO 3",
+                            country_code = "MX"
+                       }
+                    }
+        }
+        );
+    initializer.Add(
+         new ML.User
+         {
+             email = "abigail.sanchez@ejemplo.com",
+             name = "Abigail Sanchez",
+             phone = "5512345678",
+             password = "Abigail321",
+             addresses = new List<Address>
+    {
+        new Address
+        {
+            id = 1,
+            name = "HomeAddress",
+            street = "Av. Reforma 100",
+            country_code = "MX"
+        },
+        new Address
+        {
+            id = 2,
+            name = "WorkAddress",
+            street = "Insurgentes Sur 250",
+            country_code = "MX"
+        },
+        new Address
+        {
+            id = 3,
+            name = "VacationHouse",
+            street = "Ocean Drive 500",
+            country_code = "US"
+        }
+    }
+         }
+        );
+}
 
 app.Run();
