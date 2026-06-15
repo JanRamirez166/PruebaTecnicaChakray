edc58cdb1bc5d0d95ac37d81a1d47075b21bf687
Elios <eliosjb@outlook.com>
2026-06-15 14:32:57 -0600

    Agregando el modelo user en la capa ML

---
+++ ML/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class User
+    {
+        public string? id { get; set; }
+        public string? email { get; set; }
+        public string? name { get; set; }
+        public string? phone { get; set; }
+        public string? password { get; set; }
+        public string? tax_id { get; set; }
+        public string? created_at { get; set; }
+
+    }
+}
