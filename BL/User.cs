17b8365c9fefa301dccec04710b0c1f7e97bd89a
Elios <eliosjb@outlook.com>
2026-06-15 15:33:28 -0600

    Agregando el metodo Add para agregar usuarios y se agrego el metodo de GetById para validar que no exista otro usuario con el mismo id

---
+++ BL/User.cs
@@ -0,0 +1,66 @@
+﻿using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class User
+    {
+
+        public ML.Result GetById(string id)
+        {
+            ML.Result result = new ML.Result();
+
+            var query = DL.EJanBD.users.SingleOrDefault(user => user.id == id);
+
+            if (query != null)
+            {
+                result.Object = query;
+                result.Correct = true;//quiere decir que si existe ese usuario con ese ID
+            }
+            else
+            {
+                result.Correct = false;//quiere decir que no existe y se puede agregar
+            }
+
+            return result;
+        }
+
+        public ML.Result Add(ML.User user)
+        {
+            ML.Result result = new ML.Result();
+
+            //agregar el id de guid
+            user.id = Guid.NewGuid().ToString();
+
+            //validar que el usaurio no se repita por el id
+            ML.Result resultGetById = GetById(user.id);
+
+            if (!resultGetById.Correct)
+            {
+
+                DL.EJanBD.users.Add(user);
+                result.Object = user;
+                result.Correct = true;
+                result.status = 201;
+
+            }
+            else
+            {
+                result.Correct = false;
+                result.status = 500;
+                result.ErrorMessage = "Este usuario ya existe";
+            }
+
+
+            return result;
+
+        }
+
+
+    }
+}
