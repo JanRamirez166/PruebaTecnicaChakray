5ef4446c3ed4e28a969ced6781df97d76e6721ce
Elios <eliosjb@outlook.com>
2026-06-15 15:05:27 -0600

    Se agrego el modelo Result en la capa ML para retornar valores

---
+++ ML/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Result
+    {
+        public bool Correct { get; set; }
+        public string ErrorMessage { get; set; }
+        public Exception Ex { get; set; }
+        public int status { get; set; }
+        public List<User> Objects { get; set; } //GetAll
+        public object Object { get; set; } //GetById
+    }
+}
