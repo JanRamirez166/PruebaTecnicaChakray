9d10263690074760a8417d63b4f3e08cc4804f47
Elios <eliosjb@outlook.com>
2026-06-15 14:37:41 -0600

    Creando el modelo Address en la capa ML para las direcciones

---
+++ ML/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Address
+    {
+        public int? id { get; set; }
+        public string? name { get; set; }
+        public string? street { get; set; }
+        public string? country_code { get; set; }
+
+    }
+}
