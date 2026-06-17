727fb215c2ef62682b4aa0c88f3a9b10039df6b8
Elios <eliosjb@outlook.com>
2026-06-16 18:08:04 -0600

    Creando el modelo de Login que se usara en el login del sistema

---
+++ ML/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Login
+    {
+        public string? tax_id { get; set; }
+        public string? password { get; set; }
+    }
+}
