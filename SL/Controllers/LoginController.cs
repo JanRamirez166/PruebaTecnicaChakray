a283e23eefa3191e0c802b9e6eeda9d29f51a479
Elios <eliosjb@outlook.com>
2026-06-16 18:04:00 -0600

    Creando el controlador Login y agregando autenticacion jwt

---
+++ SL/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ObtenerToken")]
+        public IActionResult GenerarToken()
+        {
+            string token = GenerarJwtToken();
+            return Ok(token);
+        }
+
+        [NonAction]
+        public string GenerarJwtToken()
+        {
+            var claims = new[] {
+                new Claim(ClaimTypes.Role, "Administrador"),
+                new Claim(ClaimTypes.Name, "Elios")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ddlkfhlwuefefgksdfdsfheoiyeoyyuefk"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "yourdomain.com",
+                audience: "yourdomain.com",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
