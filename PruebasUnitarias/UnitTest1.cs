ecfb3a625299109e8a733054976b86f97837874a
Elios <eliosjb@outlook.com>
2026-06-17 16:18:48 -0600

    Agregando metodos para realizar pruebas unitarias con xUnit

--- PruebasUnitarias/UnitTest1.cs
+++ PruebasUnitarias/UnitTest1.cs
@@ -3,9 +3,29 @@ namespace PruebasUnitarias
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ValidarLosTresUsuarios()
+        {
+            // Assert
+            Assert.Equal(3, DL.EJanBD.users.Count);
+        }
+
+        [Fact]
+        public void Logearse_UsuarioYPasswordCorrectos_DebeRetornarCorrect()
         {
 
+            var login = new ML.Login
+            {
+                tax_id = "ABC850624XYZ",
+                password = "Juan456"
+            };
+
+            var blLogin = new BL.Login();
+
+            // Act
+            ML.Result result = blLogin.Logearse(login);
+
+            // Assert
+            Assert.True(result.Correct);
         }
     }
 }
